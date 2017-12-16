847f93119cce74f9309ef05b46797139714dcba1
ayylmar <34417135+ayylmar@users.noreply.github.com>
2017-12-16 13:00:46 +0100

    Refactor

---
+++ bin/lib/regions.js
@@ -0,0 +1,55 @@
+module.exports = {
+  NA: {
+    url: "http://sls.service.enmasse.com:8080/servers/list.en",
+    hostname: "sls.service.enmasse.com",
+    address: "208.67.49.148",
+    port: 8080,
+    customServers: require("./res/servers-na.json"),
+    listenHostname: "127.0.0.10"
+  },
+  EU: {
+    hostname: "web-sls.tera.gameforge.com",
+    address: "79.110.94.195",
+    port: 4566,
+    pathname: ["/servers/list.uk", "/servers/list.de", "/servers/list.fr"],
+    customServers: require("./res/servers-eu.json"),
+    listenHostname: "127.0.0.11"
+  },
+  RU: {
+    url: "http://launcher.tera-online.ru/launcher/sls/",
+    hostname: "launcher.tera-online.ru",
+    address: "91.225.237.3",
+    port: 80,
+    customServers: require("./res/servers-ru.json"),
+    listenHostname: "127.0.0.12"
+  },
+  KR: {
+    url: "http://tera.nexon.com/launcher/sls/servers/list.xml",
+    hostname: "tera.nexon.com",
+    port: 80,
+    customServers: require("./res/servers-kr.json"),
+    listenHostname: "127.0.0.13"
+  },
+  JP: {
+    url: "https://tera.pmang.jp/game_launcher/server_list.xml?usn=0",
+    hostname: "tera.pmang.jp",
+    port: 443,
+    customServers: require("./res/servers-jp.json"),
+    listenHostname: "127.0.0.14"
+  },
+  TW: {
+    url: "http://tera.mangot5.com/game/tera/serverList.xml",
+    hostname: "tera.mangot5.com",
+    port: 80,
+    customServers: require("./res/servers-tw.json"),
+    listenHostname: "127.0.0.15"
+  },
+  "EU-TEST": {
+    hostname: "devt2-web-sls.tera.gfsrv.net",
+    address: "79.110.95.152",
+    port: 4566,
+    pathname: ["/servers/list.uk", "/servers/list.de", "/servers/list.fr"],
+    customServers: require("./res/servers-eu-test.json"),
+    listenHostname: "127.0.0.16"
+  }
+};
