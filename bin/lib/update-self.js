4aa25ba659bbf3142820e337dd496aed211740f9
Caali <caali-tera@outlook.com>
2018-03-24 21:16:07 +0100

    Fixed bug in self-update script.

--- bin/lib/update-self.js
+++ bin/lib/update-self.js
@@ -10,13 +10,26 @@ const path = require("path");
 const TeraProxyAutoUpdateServer = "https://raw.githubusercontent.com/hackerman-caali/tera-proxy/master/";
 const DiscordURL = "https://discord.gg/maqBmJV";
 
+function forcedirSync(dir) {
+  const sep = path.sep;
+  const initDir = path.isAbsolute(dir) ? sep : '';
+  dir.split(sep).reduce((parentDir, childDir) => {
+    const curDir = path.resolve(parentDir, childDir);
+    try {
+      fs.mkdirSync(curDir);
+    } catch (err) {
+
+    }
+
+    return curDir;
+  }, initDir);
+}
+
 async function autoUpdateFile(file, filepath, url) {
   try {
     const updatedFile = await request({url: url, encoding: null});
 
-    let dir = path.dirname(filepath);
-    if (!fs.existsSync(dir))
-      fs.mkdirSync(dir);
+    forcedirSync(path.dirname(filepath));
     fs.writeFileSync(filepath, updatedFile);
     return [file, true];
   } catch (e) {
